49a92163cf4ba7f17b1684ab3b71241ecc5b518b
jysn007 <jysn007@gmail.com>
2026-09-06 20:25:21 +0900

    .

--- dstone-boot/bin/startApp.sh
+++ dstone-boot/bin/startApp.sh
@@ -3,7 +3,7 @@
 JAVA_OPTS="-Xms512m -Xmx1024m"
 JAR_FILE="../target/dstone-boot.war"
 
-SPRING_PROFILES_ACTIVE=-Dspring.profiles.active=dev
+SPRING_PROFILES_ACTIVE=-Dspring.profiles.active=wsl
 
 nohup java ${JAVA_OPTS} ${SPRING_PROFILES_ACTIVE} -jar ${JAR_FILE} net.dstone.boot.DstoneBootApplication > /dev/null 2>&1 &
 # java ${JAVA_OPTS} ${SPRING_PROFILES_ACTIVE} -jar ${JAR_FILE} net.dstone.boot.DstoneBootApplication 
